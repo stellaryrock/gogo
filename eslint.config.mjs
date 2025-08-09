4c2be033fb50793332c2a11a23b0827efc9640ad
stellaryrock <alohakii@naver.com>
2025-08-10 05:40:43 +0900

    setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next', 'next/typescript', 'prettier'],  
+    rules: {},
+    ignorePatterns: ['lib/generated/prisma/**'],
+  }),
+]
 
 export default eslintConfig;
